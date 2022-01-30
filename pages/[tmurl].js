15dd3139c4a68a28753e4a9886987d1c7ac354e5
erickuo5124 <erickuo5124@gmail.com>
2022-01-31 02:52:29 +0800

    feat(ga): add Google Analytics

--- pages/[tmurl].js
+++ pages/[tmurl].js
@@ -1,6 +1,8 @@
 import { useRouter } from "next/router"
 import { getOriginURL } from "../lib/notion";
 import { useEffect } from "react";
+import Head from "next/head";
+import * as ga from "../lib/ga";
 
 export async function getServerSideProps({ params }) {
   const db = await getOriginURL(params.tmurl);
@@ -13,11 +15,33 @@ export async function getServerSideProps({ params }) {
 
 export default function Redirect({ db }) {
   const { asPath } = useRouter();
-  useEffect(()=>{
+  useEffect(() => {
     const link = db.results[0].properties.Original.url
+    ga.pageview(asPath)
     window.location.href = link
   })
   return (
-    <div>Redirect to {asPath}...</div>
+    <>
+      <Head>
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
+      <div>Redirect to {asPath}...</div>
+    </>
   )
 }
