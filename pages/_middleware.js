574f936453e67c7b0970176992327f468a175f4f
erickuo5124 <erickuo5124@gmail.com>
2022-03-27 03:48:09 +0800

    refactor(_middleware): add ga

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -1,10 +1,14 @@
 import { NextResponse } from 'next/server'
 import { getOriginURL } from '../lib/notion'
+import ReactGA from 'react-ga';
 
 export async function middleware(req) {
   const { pathname } = req.nextUrl
   const decodedStr = decodeURIComponent(pathname)
-  const db = await getOriginURL(decodedStr.substring(1));
+  const db = await getOriginURL(decodedStr.substring(1))
+  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS
+  ReactGA.initialize(GA_TRACKING_ID)
+  ReactGA.pageview(decodedStr)
   if (db.results.length !== 0) {
     const link = db.results[0].properties.Original.url
     return NextResponse.redirect(link)
